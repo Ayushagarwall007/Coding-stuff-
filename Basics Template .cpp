8748c9850453a980477b382f6e4119c13d8a4a23
AYUSH AGARWAL <100485141+Ayushagarwall007@users.noreply.github.com>
2022-07-27 03:03:26 +0530

    Create Basics Template .cpp

---
+++ Basics Template .cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+#define pi pair<int,int>
+#define vi vector<int>
+#define vii vector<pi>
+#define pb push_back
+#define f first
+#define s second
+#define nl '\n'
+#define rep(i,n,s) for(int i=0;i<n;i+=s)
+#define fr(i,n,s) for(int i=1;i<=n;i+=s)
+#define dbg(x) cout<<#x<<"->"<<x<<nl;
+#define ebg(x,y) cout<<#x<<"->"<<x<<" "<<#y<<"->"<<y<<nl;
+using namespace std;
+
+//<---------------------------------Road To Pupil-------------------------------->
+	
+void solve()
+{
+
+	
+
+}
+
+int32_t main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t=1;
+	cin >> t;
+	while (t--)
+	{
+		solve();
+	}	
+	return 0;
+}
